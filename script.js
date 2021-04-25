c6de306461239ef0e9ac28e71e393ca6558b6374
Krishnakumar <krishnakumar.amritham@gmail.com>
2021-04-25 19:45:29 +0530

    upto footer completed

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let winner=null;
 const playingObjects=["paper","stone","lizard","scissor","spock"];
 var objectIndex = Math.floor(Math.random() * playingObjects.length);
 var systemInput=playingObjects[objectIndex];
+console.log(systemInput);
 function gameLogic(userInput,systemInput)
 {
-        if (arguments.includes("stone") && arguments.includes("paper"))
+        if (arguments.contains("stone") && arguments.includes("paper"))
             winner="paper"
         if (arguments.includes("stone") && arguments.includes("scissor"))
             winner="stone" 
@@ -39,5 +40,27 @@ function gameLogic(userInput,systemInput)
     {
         //system won
     }
+    console.log(winner);
 }
+//reading from index
+var rulesBtn=document.querySelector(".rulesbutton");
+var rules=document.querySelector(".rules");
+var overlay=document.querySelector(".overlay")
+var closeBtn=document.querySelector(".closerules")
+const scissors=document.querySelector(".scissors");
+const rock=document.querySelector(".rock");
+const paper=document.querySelector(".paper");
+const lizard=document.querySelector(".lizard");
+const spock=document.querySelector(".spock");
+rulesBtn.addEventListener("click",function(){
+    console.log("clicked rules")
+    rules.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+});
+closeBtn.addEventListener("click",function(){
+    rules.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+gameLogic("scissors",systemInput);
+// scissors.addEventListener("click",gameLogic("scissor",systemInput))
 
